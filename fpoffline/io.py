b79382160771b786e2272a213d2b9ee256468d49
David Kirkby <dkirkby@uci.edu>
2022-05-03 16:57:25 -0700

    wip

--- fpoffline/io.py
+++ fpoffline/io.py
@@ -2,6 +2,8 @@
 """
 import pathlib
 
+import numpy as np
+
 import astropy.time
 import astropy.table
 
@@ -21,3 +23,48 @@ def get_snapshot(timestamp=None, maxage_days=5, path='/global/cfs/cdirs/desi/eng
             table = astropy.table.Table.read(snaps)
             table.meta['name'] = snaps.name
             return table, when
+
+
+def get_index(before=None, path='/global/cfs/cdirs/desi/engineering/focalplane/PositionerIndexTable/index_files'):
+    """Locate and read the latest positioner index table.
+
+    The returned table is augmented by FIBER_ID calculated as
+
+      FIBER_ID = 500*PETAL_LOC + 25*SLITBLOCK_ID + BLOCKFIBER_ID
+
+    when SLITBLOCK_ID and BLOCKFIBER_ID are both valid, or else -1.
+
+    Parameters
+    ----------
+    before : int or None
+        When not None, restrict the search for the most recent index table
+        to dates <= before, specified as YYYYMMDD.
+
+    Returns
+    -------
+    astropy.table.Table
+    """
+    DIR = pathlib.Path(path)
+    files = sorted(DIR.glob('desi_positioner_indexes_????????.csv'), reverse=True)
+    dates = [int(file.name[-12:-4]) for file in files]
+    if before is not None:
+        dates = [date for date in dates if date <= before]
+    if len(dates) == 0:
+        raise ValueError(f'No index table found with before={before}')
+    file = DIR / f'desi_positioner_indexes_{dates[0]}.csv'
+    table = astropy.table.Table.read(file)
+    nrows = len(table)
+    fiber_id = np.full(nrows, -1, dtype=int)
+    for irow in range(nrows):
+        row = table[irow]
+        try:
+            slit, fiber = row['SLITBLOCK_ID'], row['BLOCKFIBER_ID']
+            if slit.startswith('B') and fiber.startswith('F'):
+                fiber_id[irow] = 500*int(row['PETAL_LOC']) + 25*int(slit[1:]) + int(fiber[1:])
+        except AttributeError:
+            pass
+    table['FIBER_ID'] = fiber_id
+    table['LOCATION'] = table['PETAL_LOC'] * 1000 + table['DEVICE_LOC']
+    table.sort('LOCATION')
+    table.meta['index_name'] = file.name
+    return table
--- fpoffline/scripts/endofnight.py
+++ fpoffline/scripts/endofnight.py
@@ -5,13 +5,27 @@ import traceback
 import sys
 import pathlib
 import datetime
+import math
+from ast import literal_eval as safe_eval
+
+import numpy as np
 
 import astropy.time
 
+import desimeter
+import desimeter.io
+import desimeter.transform.ptl2fp
+import desimeter.transform.pos2ptl
+import desimeter.transform.xy2qs
+
 import fpoffline.io
+import fpoffline.const
 
 
 def run(args):
+
+    print(desimeter.__file__)
+
     # raise an exception here to flag any error
     # return value is propagated to the shell
 
@@ -50,7 +64,122 @@ def run(args):
     if snap_age > 0.6:
         logging.warning(f'Snapshot is {snap_age:.1f} days old.')
 
+    # Initialize per-device summary table with metrology.
+    metrology = desimeter.io.load_metrology() # returns an astropy table, not a pandas dataframe.
+    summary = (metrology[np.isin(metrology['DEVICE_TYPE'],['POS','ETC','FIF','GIF'])][['LOCATION','X_FP','Y_FP']]
+               .group_by('LOCATION').groups.aggregate(np.mean)) # use mean of fiducial locations
+    summary.meta = {} # Do not use an OrderedDict
+    summary.meta['night'] = args.night
+    summary.meta['midnight'] = str(midnight)
+    summary.meta['snapshot'] = snapshot.meta['name']
+    summary.meta['snap_time'] = str(snap_time)
+
+    # Add indexing info. The left join will drop 5 fidicuals that are missing metrology:
+    # P074, P030, P029, P123, P058.
+    I = fpoffline.io.get_index(args.night)
+    print(f'Using index {I.meta["index_name"]}')
+    I['LOCATION'] = 1000 * I['PETAL_LOC'] + I['DEVICE_LOC']
+    summary = astropy.table.join(summary, I, keys='LOCATION', join_type='left')
+    summary.sort('LOCATION')
+
+    # Add calibration info from the snapshot.
+    summary = reduce_snapshot(snapshot, summary)
+
+    # Compute interpolated constants to transform from flatXY to ptlXY.
+    s0 = np.hypot(summary['OFFSET_X'], summary['OFFSET_Y'])
+    r0 = desimeter.transform.xy2qs.s2r(s0)
+    summary['R0_OVER_S0'] = r0 / s0
+
+    # Save the summary table as ECSV (so the metadata is included)
+    # TODO: round float values
+    summary.meta = dict(summary.meta) # Don't use an OrderedDict
+    summary.write(output / f'fp-{args.night}.ecsv', overwrite=True)
+
     return 0
+
+
+def reduce_snapshot(snapshot, summary):
+    """Utility function used to merge snapshot calibration and keepout info into the summary table and its metadata.
+    """
+    canonical = lambda k: np.round(safe_eval(k), 3)
+
+    cols = (
+        'LENGTH_R1', 'LENGTH_R2', 'OFFSET_T', 'OFFSET_P', 'OFFSET_X', 'OFFSET_Y', 'PHYSICAL_RANGE_T', 'PHYSICAL_RANGE_P',
+        'DEVICE_CLASSIFIED_NONFUNCTIONAL', 'FIBER_INTACT', 'LOCATION')
+    reduced = astropy.table.Table(snapshot[cols])
+    reduced.meta = {}
+    reduced.sort('LOCATION')
+
+    keepouts_t, keepout_t_idx = get_keepouts(snapshot, 'T', canonical)
+    keepouts_p, keepout_p_idx = get_keepouts(snapshot, 'P', canonical)
+    keepouts = dict(theta=keepouts_t.tolist(), phi=keepouts_p.tolist())
+
+    # Transform GFA and PTL keepouts to focal-plane coords for each petal.
+    gfa_x, gfa_y = desimeter.transform.pos2ptl.flat2ptl(
+        *canonical(snapshot.meta['keepout_GFA']))
+    ptl_x, ptl_y = desimeter.transform.pos2ptl.flat2ptl(
+        *canonical(snapshot.meta['keepout_PTL']))
+    gfa, ptl = [], []
+    for petal_loc in range(10):
+        x, y, _ = desimeter.transform.ptl2fp.ptl2fp(petal_loc, gfa_x, gfa_y)
+        gfa.append(np.array([x, y]).tolist())
+        x, y, _ = desimeter.transform.ptl2fp.ptl2fp(petal_loc, ptl_x, ptl_y)
+        ptl.append(np.array([x, y]).tolist())
+    keepouts['gfa'] = gfa
+    keepouts['ptl'] = ptl
+
+    # Save the indices of the keepouts used by each device.
+    reduced['KEEPOUT_T'] = keepout_t_idx
+    reduced['KEEPOUT_P'] = keepout_p_idx
+
+    # Merge with the summary. The snapshot has no entries for the fidicuals, so their new fields
+    # will be empty after the left join.
+    summary = astropy.table.join(summary, reduced, keys='LOCATION', join_type='left')
+
+    summary.meta['keepout'] = keepouts
+
+    petals = []
+    for petal_loc in range(10):
+        petal_id = fpoffline.const.PETAL_ID_MAP[petal_loc]
+        alignment = snapshot.meta['PETAL_ALIGNMENTS'][petal_id]
+        gamma = alignment['gamma']
+        petals.append(dict(
+            Tx=alignment['Tx'], Ty=alignment['Ty'],
+            cosGamma=math.cos(gamma), sinGamma=math.sin(gamma)))
+    summary.meta['petals'] = petals
+
+    return summary
+
+
+def get_keepouts(snap, arm, canonical):
+
+    nsnap = len(snap)
+    keepouts = [ canonical(snap.meta[f'general_keepout_{arm}']) ]
+    keepout_idx = np.full(nsnap, -1, int)
+
+    for j, row in enumerate(snap):
+        k = canonical(row[f'KEEPOUT_{arm}'])
+        for i, ki in enumerate(keepouts):
+            if np.array_equal(k, ki):
+                keepout_idx[j] = i
+                break
+        if keepout_idx[j] == -1:
+            keepout_idx[j] = len(keepouts)
+            keepouts.append(k)
+
+    logging.info(f'Found {len(keepouts)} unique {arm} keepouts for {nsnap} devices')
+    return np.array(keepouts), keepout_idx
+
+
+#def ptl2fp(x_ptl, y_ptl, petal_locs):
+#    """Transform from petal flat (x,y) to focal-plane (x,y) with desimeter translate and rotate.
+#    """
+#    x_fp, y_fp = np.zeros(shape=(2,)+x_ptl.shape)
+#    for petal_loc in np.unique(petal_locs):
+#        sel = (petal_locs == petal_loc)
+#        x_fp[sel], y_fp[sel], _ = desimeter.transform.ptl2fp.ptl2fp(
+#            petal_loc, x_ptl[sel], y_ptl[sel])
+#    return x_fp, y_fp
 
 
 def main():
