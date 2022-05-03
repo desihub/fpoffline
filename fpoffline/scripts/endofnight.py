870dc350f8c7e1539ec36ce6b27c01efabc80ff5
David Kirkby <dkirkby@uci.edu>
2022-05-03 15:11:12 -0700

    Start end of night script

---
+++ fpoffline/scripts/endofnight.py
@@ -0,0 +1,100 @@
+import argparse
+import logging
+import pdb
+import traceback
+import sys
+import pathlib
+import datetime
+
+import astropy.time
+
+
+def run(args):
+    # raise an exception here to flag any error
+    # return value is propagated to the shell
+
+    if args.night is None:
+        # Use yesterday's date by default.
+        args.night = int((datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y%m%d'))
+        print(f'Using default night {args.night}')
+
+    # Check we have a valid parent path.
+    if not args.parent_dir.exists():
+        raise RuntimeError(f'Non-existent parent_dir: {args.parent_dir}')
+
+    # Create a night subdirectory if necessary.
+    output = args.parent_dir / str(args.night)
+    if not output.exists():
+        print(f'Creating {output}')
+        output.mkdir()
+
+    # Configure logging.
+    if args.debug:
+        level = logging.DEBUG
+    elif args.verbose:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    if args.logpath is None:
+        args.logpath = output / f'endofnight-{args.night}.log'
+    print(f'Saving log to {args.logpath}')
+    logging.basicConfig(filename=args.logpath, level=level,
+        format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y%m%d %H:%M:%S')
+
+    # Calculate local midnight for this observing midnight.
+    N = str(args.night)
+    year, month, day = int(N[0:4]), int(N[4:6]), int(N[6:8])
+    midnight = astropy.time.Time(datetime.datetime(year, month, day, 12) + datetime.timedelta(hours=19))
+    logging.info(f'Local midnight on {N} is at {midnight}')
+
+    # Calculate local noons before and after local midnight.
+    twelve_hours = astropy.time.TimeDelta(12 * 3600, format='sec')
+    noon_before = midnight - twelve_hours
+    noon_after = midnight + twelve_hours
+
+    return 0
+
+
+def main():
+    # https://docs.python.org/3/howto/argparse.html
+    parser = argparse.ArgumentParser(
+        description='Run the focal-plane end of night analysis',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--night', type=str, metavar='YYYYMMDD',
+        help='night to process or use the most recent night if not specified')
+    parser.add_argument('--overwrite', action='store_true',
+        help='overwrite any existing output files')
+    parser.add_argument('--front-id', type=int, metavar='NNNNNNNN',
+        help='exposure ID to use for the front-illuminated image')
+    parser.add_argument('--back-id', type=int, metavar='NNNNNNNN',
+        help='exposure ID to use for the back-illuminated image')
+    parser.add_argument('--park-id', type=int, metavar='NNNNNNNN',
+        help='exposure ID to use for the park robots script image')
+    parser.add_argument('--parent-dir', type=pathlib.Path, metavar='PATH',
+        default=pathlib.Path('/global/cfs/cdirs/desi/engineering/focalplane/endofnight'),
+        help='parent directory for per-night output directories')
+    parser.add_argument('-v', '--verbose', action='store_true',
+        help='provide verbose output on progress')
+    parser.add_argument('--debug', action='store_true',
+        help='provide verbose and debugging output')
+    parser.add_argument('--logpath', type=pathlib.Path, metavar='PATH',
+        help='Path where logging output should be written')
+    parser.add_argument('--traceback', action='store_true',
+        help='print traceback and enter debugger after an exception')
+    args = parser.parse_args()
+
+    try:
+        retval = run(args)
+        sys.exit(retval)
+    except Exception as e:
+        if args.traceback:
+            # https://stackoverflow.com/questions/242485/starting-python-debugger-automatically-on-error
+            extype, value, tb = sys.exc_info()
+            traceback.print_exc()
+            pdb.post_mortem(tb)
+        else:
+            print(e)
+            sys.exit(-1)
+
+if __name__ == '__main__':
+    main()
