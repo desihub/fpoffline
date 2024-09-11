074dc2ac7f4d015a4a1c1f018b32273c671559fb
David Kirkby <dkirkby@uci.edu>
2024-09-11 11:51:25 -0700

    drop nrows in compression

--- fpoffline/hwtable.py
+++ fpoffline/hwtable.py
@@ -1,5 +1,6 @@
 """Load and interpret positioner hardware tables.
 """
+
 import os
 import pathlib
 import json
@@ -12,7 +13,9 @@ import desimeter.transform.pos2ptl
 import desimeter.transform.ptl2fp
 
 
-def load_hwtable(expid, petal_id=None, exp_iter=None, path=None, verbose=False, compress=True):
+def load_hwtable(
+    expid, petal_id=None, exp_iter=None, path=None, verbose=False, compress=True
+):
     """Load the positioner hardware move table(s) for the specified exposure.
 
     Parameters
@@ -43,24 +46,24 @@ def load_hwtable(expid, petal_id=None, exp_iter=None, path=None, verbose=False, 
     """
     # Get the path to use.
     if path is None:
-        if os.getenv('NERSC_HOST') is not None:
-            path = '/global/cfs/cdirs/desi/engineering/focalplane/hwtables'
-        elif os.getenv('DOS_HOST') is not None:
+        if os.getenv("NERSC_HOST") is not None:
+            path = "/global/cfs/cdirs/desi/engineering/focalplane/hwtables"
+        elif os.getenv("DOS_HOST") is not None:
             if expid >= 91163:
-                path = '/global/cfs/cdirs/desi/engineering/focalplane/hwtables'
+                path = "/global/cfs/cdirs/desi/engineering/focalplane/hwtables"
             else:
-                path = '/data/msdos/focalplane/fp_temp_files/'
+                path = "/data/msdos/focalplane/fp_temp_files/"
     if path is None:
-        raise ValueError('Must specify a path except at NERSC or KPNO.')
+        raise ValueError("Must specify a path except at NERSC or KPNO.")
     path = pathlib.Path(path)
     if not path.exists():
         raise ValueError('Invalid path: "{path}".')
     if verbose:
         print(f'Using path: "{path}".')
     # Look for any files associated with this exposure.
-    files = sorted(path.glob(f'hwtables_ptlid??_{expid}_*.csv'))
+    files = sorted(path.glob(f"hwtables_ptlid??_{expid}_*.csv"))
     if len(files) == 0:
-        raise ValueError(f'No files found for expid {expid}.')
+        raise ValueError(f"No files found for expid {expid}.")
     # Decode petal_id and exp_iter from each filename.
     petal_ids, exp_iters = [], []
     expstr = str(expid)
@@ -68,103 +71,160 @@ def load_hwtable(expid, petal_id=None, exp_iter=None, path=None, verbose=False, 
         name = file.name
         petal_ids.append(int(name[14:16]))
         lo = name.index(expstr) + len(expstr) + 1
-        hi = name.index('_', lo)
+        hi = name.index("_", lo)
         exp_iters.append(int(name[lo:hi]) if hi > lo else 0)
     if verbose:
-        print(f'Found petal_ids {set(petal_ids)} and exp_iters {set(exp_iters)}.')
+        print(f"Found petal_ids {set(petal_ids)} and exp_iters {set(exp_iters)}.")
     # Are the request petal_id and exp_iter available?
     if petal_id is not None and exp_iter is not None:
         if (petal_id, exp_iter) not in zip(petal_ids, exp_iters):
-            raise ValueError(f'Requested petal_id={petal_id} and exp_iter={exp_iter} not found for expid {expid}.')
+            raise ValueError(
+                f"Requested petal_id={petal_id} and exp_iter={exp_iter} not found for expid {expid}."
+            )
     elif petal_id is not None:
         if petal_id not in petal_ids:
-            raise ValueError(f'Requested petal_id={petal_id} not found for expid {expid}.')
+            raise ValueError(
+                f"Requested petal_id={petal_id} not found for expid {expid}."
+            )
     elif exp_iter is not None:
         if exp_iter not in exp_iters:
-            raise ValueError(f'Requested exp_iter={exp_iter} not found for expid {expid}.')
+            raise ValueError(
+                f"Requested exp_iter={exp_iter} not found for expid {expid}."
+            )
     # Read CSV files.
     dfs = []
-    for i,file in enumerate(files):
+    for i, file in enumerate(files):
         if petal_id not in (petal_ids[i], None):
             continue
         if exp_iter not in (exp_iters[i], None):
             continue
         if verbose:
-            print(f'Reading {file}')
+            print(f"Reading {file}")
         df = pd.read_csv(file)
         # Add petal_id, exp_iter columns.
-        df['petal_id'] = petal_ids[i]
-        df['exp_iter'] = exp_iters[i]
+        df["petal_id"] = petal_ids[i]
+        df["exp_iter"] = exp_iters[i]
         dfs.append(df)
     # Combine all CSVs.
     df = pd.concat(dfs, ignore_index=True)
     # Sort by increasing exp_iter
-    df.sort_values('exp_iter', inplace=True, ignore_index=True)
+    df.sort_values("exp_iter", inplace=True, ignore_index=True)
     if compress:
         # Apply optional compression steps
-        df = df.drop(columns=['busid','canid','petal_id','move_time','total_time','required','failed_to_send'])
-        for col in 'motor_steps_P', 'motor_steps_T', 'speed_mode_T', 'speed_mode_P', 'postpause':
-            df[col] = df[col].str.replace(', ', ',')
-        for col in 'speed_mode_T', 'speed_mode_P':
+        df = df.drop(
+            columns=[
+                "busid",
+                "canid",
+                "petal_id",
+                "move_time",
+                "total_time",
+                "required",
+                "failed_to_send",
+                "nrows",
+            ]
+        )
+        for col in (
+            "motor_steps_P",
+            "motor_steps_T",
+            "speed_mode_T",
+            "speed_mode_P",
+            "postpause",
+        ):
+            df[col] = df[col].str.replace(", ", ",")
+        for col in "speed_mode_T", "speed_mode_P":
             df[col] = df[col].str.replace("'creep'", "0").str.replace("'cruise'", "1")
     return df
 
+
 class Scheduler:
-    """Initialize a new move scheduler for a positioner.
-    """
-    CLOCK = 18000                 # tick frequency in Hz
-    PAUSE = 18                    # clock ticks per unit of pause
-    CREEP_STEP = 0.1              # motor rotation in degrees for a single creep step
-    CRUISE_RATIO = 33             # ratio of cruise and creep motor rotations for a single step
-    GEAR_RATIO = (46.0/14.0+1)**4 # Namiki "337:1" gear box (GEAR_TYPE_P,T in constants db)
+    """Initialize a new move scheduler for a positioner."""
+
+    CLOCK = 18000  # tick frequency in Hz
+    PAUSE = 18  # clock ticks per unit of pause
+    CREEP_STEP = 0.1  # motor rotation in degrees for a single creep step
+    CRUISE_RATIO = 33  # ratio of cruise and creep motor rotations for a single step
+    GEAR_RATIO = (
+        46.0 / 14.0 + 1
+    ) ** 4  # Namiki "337:1" gear box (GEAR_TYPE_P,T in constants db)
 
     default_const = dict(
-        CREEP_PERIOD=2, SPINUPDOWN_PERIOD=12, MOTOR_CCW_DIR_T=-1, MOTOR_CCW_DIR_P=-1,
-        GEAR_TYPE_T='namiki', GEAR_TYPE_P='namiki')
+        CREEP_PERIOD=2,
+        SPINUPDOWN_PERIOD=12,
+        MOTOR_CCW_DIR_T=-1,
+        MOTOR_CCW_DIR_P=-1,
+        GEAR_TYPE_T="namiki",
+        GEAR_TYPE_P="namiki",
+    )
 
     default_calib = dict(
-        length_r1=3.0, length_r2=3.0, offset_t=0.0, offset_p=0.0,
-        offset_x=0.0, offset_y=0.0, gear_calib_t=1.0, gear_calib_p=1.0)
+        length_r1=3.0,
+        length_r2=3.0,
+        offset_t=0.0,
+        offset_p=0.0,
+        offset_x=0.0,
+        offset_y=0.0,
+        gear_calib_t=1.0,
+        gear_calib_p=1.0,
+    )
 
     class Motor:
-        """
-        """
+        """ """
+
         def __init__(self, CREEP_PERIOD=2, SPINUPDOWN_PERIOD=12, ccw=-1, ratio=1):
             self.CREEP_PERIOD = CREEP_PERIOD
             self.SPINUPDOWN_PERIOD = SPINUPDOWN_PERIOD
-            self.dw = ccw * ratio * Scheduler.CREEP_STEP * Scheduler.CLOCK / (self.CREEP_PERIOD * Scheduler.GEAR_RATIO)
+            self.dw = (
+                ccw
+                * ratio
+                * Scheduler.CREEP_STEP
+                * Scheduler.CLOCK
+                / (self.CREEP_PERIOD * Scheduler.GEAR_RATIO)
+            )
             self.increment = []
             self.duration = []
             self.nticks = 0
 
         def creep(self, steps):
-            if steps==0: return
-            direction, nsteps = (1,steps) if steps > 0 else (-1,-steps)
+            if steps == 0:
+                return
+            direction, nsteps = (1, steps) if steps > 0 else (-1, -steps)
             self.increment.append(direction)
             self.duration.append(self.CREEP_PERIOD * nsteps)
             self.nticks += self.duration[-1]
 
         def cruise(self, steps):
-            if steps==0: return
-            direction, nsteps = (1,steps) if steps > 0 else (-1,-steps)
+            if steps == 0:
+                return
+            direction, nsteps = (1, steps) if steps > 0 else (-1, -steps)
             nupdown = Scheduler.CRUISE_RATIO
-            spin_up_down = [(i + 1) * direction * self.CREEP_PERIOD for i in range(nupdown)]
+            spin_up_down = [
+                (i + 1) * direction * self.CREEP_PERIOD for i in range(nupdown)
+            ]
             self.increment.extend(spin_up_down + spin_up_down[-1:] + spin_up_down[::-1])
-            self.duration.extend([self.SPINUPDOWN_PERIOD] * nupdown + [nsteps] + [self.SPINUPDOWN_PERIOD] * nupdown)
+            self.duration.extend(
+                [self.SPINUPDOWN_PERIOD] * nupdown
+                + [nsteps]
+                + [self.SPINUPDOWN_PERIOD] * nupdown
+            )
             self.nticks += 2 * self.SPINUPDOWN_PERIOD * Scheduler.CRUISE_RATIO + nsteps
 
         def pause(self, steps, unit=None):
             unit = unit or Scheduler.PAUSE
-            if steps < 0: raise ValueError('Invalid steps < 0.')
-            if steps == 0: return
+            if steps < 0:
+                raise ValueError("Invalid steps < 0.")
+            if steps == 0:
+                return
             self.increment.append(0)
             self.duration.append(steps * unit)
             self.nticks += self.duration[-1]
 
         def move(self, steps, mode, pause):
-            if mode == 'creep': self.creep(steps)
-            elif mode == 'cruise': self.cruise(steps)
-            else: raise ValueError(f'Invalid move mode "{mode}".')
+            if mode == "creep":
+                self.creep(steps)
+            elif mode == "cruise":
+                self.cruise(steps)
+            else:
+                raise ValueError(f'Invalid move mode "{mode}".')
             self.pause(pause)
             assert len(self.increment) == len(self.duration)
 
@@ -184,7 +244,7 @@ class Scheduler:
             calib = calib.iloc[0].to_dict()
         except AttributeError:
             pass
-        if const['GEAR_TYPE_T'] != 'namiki' or const['GEAR_TYPE_P'] != 'namiki':
+        if const["GEAR_TYPE_T"] != "namiki" or const["GEAR_TYPE_P"] != "namiki":
             raise ValueError('Only "namiki" gears are supported.')
         self.const = const
         self.calib = calib
@@ -204,23 +264,37 @@ class Scheduler:
         except AttributeError:
             pass
         # Convert args from strings to arrays of python types.
-        args = [json.loads(hwtable[name].replace("'", '"')) for name in
-                ('motor_steps_T', 'speed_mode_T', 'motor_steps_P', 'speed_mode_P', 'postpause')]
+        args = [
+            json.loads(hwtable[name].replace("'", '"'))
+            for name in (
+                "motor_steps_T",
+                "speed_mode_T",
+                "motor_steps_P",
+                "speed_mode_P",
+                "postpause",
+            )
+        ]
         # Check that arrays have matching lengths.
         try:
             lens = [len(arg) for arg in args]
             assert min(lens) == max(lens)
         except (TypeError, AssertionError):
-            raise RuntimeError('Arguments must be arrays of the same length.')
+            raise RuntimeError("Arguments must be arrays of the same length.")
 
         # Schedule the move table.
         self.motor_T = Scheduler.Motor(
-            self.const['CREEP_PERIOD'], self.const['SPINUPDOWN_PERIOD'],
-            self.const['MOTOR_CCW_DIR_T'], self.calib['gear_calib_t'])
+            self.const["CREEP_PERIOD"],
+            self.const["SPINUPDOWN_PERIOD"],
+            self.const["MOTOR_CCW_DIR_T"],
+            self.calib["gear_calib_t"],
+        )
         self.motor_P = Scheduler.Motor(
-            self.const['CREEP_PERIOD'], self.const['SPINUPDOWN_PERIOD'],
-            self.const['MOTOR_CCW_DIR_P'], self.calib['gear_calib_p'])
-        for (steps_T, mode_T, steps_P, mode_P, pause) in zip(*args):
+            self.const["CREEP_PERIOD"],
+            self.const["SPINUPDOWN_PERIOD"],
+            self.const["MOTOR_CCW_DIR_P"],
+            self.calib["gear_calib_p"],
+        )
+        for steps_T, mode_T, steps_P, mode_P, pause in zip(*args):
             self.motor_T.move(steps_T, mode_T, pause)
             self.motor_P.move(steps_P, mode_P, pause)
             # Add extra delay to one motor, if needed, to keep both in synch.
@@ -236,14 +310,13 @@ class Scheduler:
         self.finalized = True
 
     def get_path_for_move(self, move):
-        """Wrapper for get_path that uses a move record for the final angles to use.
-        """
+        """Wrapper for get_path that uses a move record for the final angles to use."""
         # Convert pandas series to dictionary if necessary.
         try:
             move = move.iloc[0].to_dict()
         except AttributeError:
             pass
-        return self.get_path(move['pos_t'], move['pos_p'], external=False)
+        return self.get_path(move["pos_t"], move["pos_p"], external=False)
 
     def get_path(self, t_final, p_final, external=True, max_tstep=0.05):
         """Calculate the theta and phi arm paths in various coordinate systems.
@@ -264,42 +337,70 @@ class Scheduler:
             the (x,y) components might have large jumps.
         """
         if not self.finalized:
-            raise RuntimeError('Call the plan() method before calculating a path.')
+            raise RuntimeError("Call the plan() method before calculating a path.")
         if not external:
             # Convert from internal to external angles.
-            t_final = desimeter.transform.pos2ptl.int2ext(t_final, self.calib['offset_t'])
-            p_final = desimeter.transform.pos2ptl.int2ext(p_final, self.calib['offset_p'])
+            t_final = desimeter.transform.pos2ptl.int2ext(
+                t_final, self.calib["offset_t"]
+            )
+            p_final = desimeter.transform.pos2ptl.int2ext(
+                p_final, self.calib["offset_p"]
+            )
         # The motor rotations are tabulated on independent time grids so combine them
         # to a common grid now.
         if max_tstep is None:
             # Take the union of the two motor grids.
-            self.time = np.unique(np.concatenate((self.motor_T.time, self.motor_P.time)))
+            self.time = np.unique(
+                np.concatenate((self.motor_T.time, self.motor_P.time))
+            )
         else:
             # Use the smallest tstep < max_tstep that evenly divides this motion's duration.
             nt = int(np.ceil(self.duration / max_tstep)) + 1
             self.time = np.linspace(0, self.duration, nt)
             assert self.time[1] <= max_tstep
         # Interpolate the angles onto the same grid, adjusted to end at t_final, p_final.
-        self.t_ext = np.interp(self.time, self.motor_T.time,
-            t_final + self.motor_T.angle - self.motor_T.angle[-1])
-        self.p_ext = np.interp(self.time, self.motor_P.time,
-            p_final + self.motor_P.angle - self.motor_P.angle[-1])
+        self.t_ext = np.interp(
+            self.time,
+            self.motor_T.time,
+            t_final + self.motor_T.angle - self.motor_T.angle[-1],
+        )
+        self.p_ext = np.interp(
+            self.time,
+            self.motor_P.time,
+            p_final + self.motor_P.angle - self.motor_P.angle[-1],
+        )
         # Convert from angles to petal local (x,y).
         self.x_loc, self.y_loc = desimeter.transform.pos2ptl.ext2loc(
-            self.t_ext, self.p_ext, self.calib['length_r1'], self.calib['length_r2'])
+            self.t_ext, self.p_ext, self.calib["length_r1"], self.calib["length_r2"]
+        )
         # (xt,yt) refers to the phi axis at the end of the theta arm, calculated by setting r2=0.
         self.xt_loc, self.yt_loc = desimeter.transform.pos2ptl.ext2loc(
-            self.t_ext, self.p_ext, self.calib['length_r1'], 0)
+            self.t_ext, self.p_ext, self.calib["length_r1"], 0
+        )
         # Convert from local (x,y) to flat (x,y).
-        self.x_flat = desimeter.transform.pos2ptl.loc2flat(self.x_loc, self.calib['offset_x'])
-        self.y_flat = desimeter.transform.pos2ptl.loc2flat(self.y_loc, self.calib['offset_y'])
-        self.xt_flat = desimeter.transform.pos2ptl.loc2flat(self.xt_loc, self.calib['offset_x'])
-        self.yt_flat = desimeter.transform.pos2ptl.loc2flat(self.yt_loc, self.calib['offset_y'])
+        self.x_flat = desimeter.transform.pos2ptl.loc2flat(
+            self.x_loc, self.calib["offset_x"]
+        )
+        self.y_flat = desimeter.transform.pos2ptl.loc2flat(
+            self.y_loc, self.calib["offset_y"]
+        )
+        self.xt_flat = desimeter.transform.pos2ptl.loc2flat(
+            self.xt_loc, self.calib["offset_x"]
+        )
+        self.yt_flat = desimeter.transform.pos2ptl.loc2flat(
+            self.yt_loc, self.calib["offset_y"]
+        )
         # Convert from flat (x,y) to petal (x,y).
-        self.x_ptl, self.y_ptl = desimeter.transform.pos2ptl.flat2ptl(self.x_flat, self.y_flat)
-        self.xt_ptl, self.yt_ptl = desimeter.transform.pos2ptl.flat2ptl(self.xt_flat, self.yt_flat)
+        self.x_ptl, self.y_ptl = desimeter.transform.pos2ptl.flat2ptl(
+            self.x_flat, self.y_flat
+        )
+        self.xt_ptl, self.yt_ptl = desimeter.transform.pos2ptl.flat2ptl(
+            self.xt_flat, self.yt_flat
+        )
         # Convert from petal (x,y) to focal plane (x,y,z).
         self.x_fp, self.y_fp, self.z_fp = desimeter.transform.ptl2fp.ptl2fp(
-            self.calib['petal_loc'], self.x_ptl, self.y_ptl)
+            self.calib["petal_loc"], self.x_ptl, self.y_ptl
+        )
         self.xt_fp, self.yt_fp, self.zt_fp = desimeter.transform.ptl2fp.ptl2fp(
-            self.calib['petal_loc'], self.xt_ptl, self.yt_ptl)
+            self.calib["petal_loc"], self.xt_ptl, self.yt_ptl
+        )
